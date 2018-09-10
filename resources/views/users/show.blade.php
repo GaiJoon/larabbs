32b1c31854165358ce090672ea434a4ad665aabb
vagrant <vagrant@homestead>
2018-09-10 07:40:00 +0000

    Setup default home page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -21,7 +21,7 @@
                         <hr>
                         <h4><strong>注册于</strong></h4>
                         <p>{{$user->created_at->diffForHumans()}}</p>
-                        <hr>    
+                        <hr>
                         <h4><strong>最后活跃</strong></h4>
                         <p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
                     </div>
