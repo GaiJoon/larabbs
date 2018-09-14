4953265b1652bfa6a49523a336bc38d8b93a0822
vagrant <vagrant@homestead>
2018-09-14 08:45:11 +0000

    topics show

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -39,6 +39,10 @@ class TopicsController extends Controller
 
         return $this->response->paginator($topics, new TopicTransformer());
     }
+    public function show(Topic $topic)
+    {
+        return $this->response->item($topic, new TopicTransformer());
+    }
 
     public function store(TopicRequest $request, Topic $topic)
     {
